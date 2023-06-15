b03c9ebba6afeb235d4c81782fa914d25c5e0cfb
Ebenezer Jackson <asajaxx@icloud.com>
2023-06-15 05:21:32 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 
 /**
  * main - prints "progamming is like building a multilingual 
- * puzzle, followed by a newline."
- * Return 0
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multlingual puzzle");
 	return (0);
+
 }
