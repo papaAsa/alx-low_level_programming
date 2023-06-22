25fe72ec552462e072dc010e0cdbb1d8911da5f3
Ebenezer Jackson <asajaxx@icloud.com>
2023-06-22 11:42:38 -0700

    done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,12 +9,22 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n')
+		_putchar('\n');
 	}
 	else
 	{
 		int i, j;
 
-		for (i = -; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
