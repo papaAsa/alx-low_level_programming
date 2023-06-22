e447abb50e8ee57e3998fe06084f71bcaebaa9f8
Ebenezer Jackson <asajaxx@icloud.com>
2023-06-22 13:41:27 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,32 +8,31 @@
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
-			printf(" Fizz");
+			printf("FizzBuzz");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (x % 3 == 0)
 		{
-			printf(" Buzz");
+			printf("Fizz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (x % 5 == 0)
 		{
-			printf(" FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
-
 	return (0);
 }
