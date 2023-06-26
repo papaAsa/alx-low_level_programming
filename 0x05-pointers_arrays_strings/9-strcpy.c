246d9722d0fc04f722df6978625b66e430bac917
Ebenezer Jackson <asajaxx@icloud.com>
2023-06-26 13:11:47 -0700

    done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,16 +3,17 @@
 /**
  * char*_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
- * @scr: copy from
+ * @src: copy from
  * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest [i++] = '\0';
+	dest[i++] = '\0';
 	return (dest);
 }
