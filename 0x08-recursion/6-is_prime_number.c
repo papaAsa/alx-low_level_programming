7da0e6429af669146dacbef9844de348d9a00f07
Ebenezer Jackson <asajaxx@icloud.com>
2023-07-05 02:03:00 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,7 @@ int actual_prime(int n, int i);
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to be evaluated
- * return: 1 if n is a prime number, 0 if not
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
